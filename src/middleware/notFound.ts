c92139ff504992d3fea069e66d714e7757284994
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-01 14:46:48 +0600

    feat: update notFound middleware to use AppError for error handling

--- src/middleware/notFound.ts
+++ src/middleware/notFound.ts
@@ -1,9 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "../constants/statusCodes.js";
+import { AppError } from "../errors/AppError.js";
 
-export const notFound = (req: Request, res: Response, _next: NextFunction) => {
-    res.status(StatusCodes.NOT_FOUND).json({
-        success: false,
-        message: `Not Found - ${req.originalUrl}`,
-    });
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+    next(new AppError(`Not Found - ${req.originalUrl}`, StatusCodes.NOT_FOUND))
 };
