a757dfbdd119b64e6e1bcb74a05ab48a4d37020c
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-02 09:42:48 +0600

    feat: refactor catchAsync to use AsyncHandler type for improved type safety

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,7 +1,12 @@
-import type { Request, Response, NextFunction, RequestHandler } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 
-export const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
+type AsyncHandler = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => Promise<void>;
+
+export const catchAsync = (fn: AsyncHandler) =>
+  (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err) => next(err));
   };
-};
