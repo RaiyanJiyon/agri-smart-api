8f6a48310ab1e8756e1b3fd0fdbb7e8edcc1e52c
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-02 07:53:26 +0600

    feat: add user role and status constants, and define IUser interface for authentication

---
+++ src/modules/auth/auth.constant.ts
@@ -0,0 +1,10 @@
+export const USER_ROLE = {
+    FARMER: "farmer",
+    ADMIN: "admin",
+} as const;
+
+export const USER_STATUS = {
+    ACTIVE: "active",
+    INACTIVE: "inactive",
+    BLOCKED: "blocked",
+} as const;
