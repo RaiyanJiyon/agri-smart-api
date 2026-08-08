c11425426ee30d3dd9a262979c55f6c737e53fe4
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-08 16:09:07 +0600

    feat: implement utility functions for user and farm ObjectId validation

--- src/app/modules/farm/farm.utils.ts
+++ src/app/modules/farm/farm.utils.ts
@@ -1,3 +1,26 @@
-/**
- * Farm utils will be implement in here.
- */
+import  { Types } from "mongoose";
+import { ApiError } from "../../shared/errors/AppError.js";
+import { HTTP_STATUS } from "../../shared/constants/httpStatus.js";
+import type { Request } from "express";
+
+export const getUserObjectId = (req: Pick<Request, "user">): Types.ObjectId => {
+  const userId = req.user?.userId;
+
+  if (!userId) {
+    throw new ApiError(HTTP_STATUS.UNAUTHORIZED, "User not found.");
+  }
+
+  if (!Types.ObjectId.isValid(userId)) {
+    throw new ApiError(HTTP_STATUS.UNAUTHORIZED, "Invalid user identity.");
+  }
+
+  return new Types.ObjectId(userId);
+};
+
+export const getFarmObjectId = (farmId: string): Types.ObjectId => {
+  if (!Types.ObjectId.isValid(farmId)) {
+    throw new ApiError(HTTP_STATUS.BAD_REQUEST, "Invalid farm ID.");
+  }
+
+  return new Types.ObjectId(farmId);
+};
