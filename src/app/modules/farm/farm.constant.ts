13ccf857fc2b91ad805fead7889c96b6cb894bea
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-08 12:16:57 +0600

    feat: add farm area units constants and update module files with proper formatting

--- src/app/modules/farm/farm.constant.ts
+++ src/app/modules/farm/farm.constant.ts
@@ -1,3 +1,8 @@
-/**
- * Farm constants will be implemented in here.
- */
+export const FARM_AREA_UNITS = {
+  ACRE: 'acre',
+  HECTARE: 'hectare',
+  BIGHAA: 'bigha',
+  KATHA: 'katha',
+} as const;
+
+export const FARM_AREA_UNIT_VALUES = Object.values(FARM_AREA_UNITS);
