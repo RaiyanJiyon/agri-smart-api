33e653c585c020c4bfa249b493848b33755dba77
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-14 12:33:50 +0600

    feat: add updateById method to DiseaseDetectionRepository

--- src/app/modules/disease-detection/disease-detection.repository.ts
+++ src/app/modules/disease-detection/disease-detection.repository.ts
@@ -1,4 +1,4 @@
-import type { Types } from 'mongoose';
+import type { Types, UpdateQuery } from 'mongoose';
 import type { DiseaseReport } from './disease-detection.interface.js';
 import { DiseaseReportModel } from './disease-detection.model.js';
 
@@ -36,9 +36,20 @@ const deleteByIdAndUserId = async (
   }).lean<DiseaseReport | null>();
 };
 
+const updateById = async (
+  diseaseReportId: Types.ObjectId,
+  update: UpdateQuery<DiseaseReport>
+): Promise<DiseaseReport | null> => {
+  return DiseaseReportModel.findByIdAndUpdate(diseaseReportId, update, {
+    new: true,
+    runValidators: true,
+  }).lean<DiseaseReport | null>();
+};
+
 export const DiseaseDetectionRepository = {
   create,
   findByUserId,
   findByIdAndUserId,
   deleteByIdAndUserId,
+  updateById,
 };
