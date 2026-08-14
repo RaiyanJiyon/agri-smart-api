46477ff78b6b1613d9c0055b1032271bc1640423
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-14 11:44:39 +0600

    feat: add interfaces for disease detection and crop recommendation job data

---
+++ src/app/shared/queue/queue.interface.ts
@@ -0,0 +1,17 @@
+import type { Types } from 'mongoose';
+import type { CropRecommendationInput } from '../../modules/crop-recommendations/crop-recommendation.interface.js';
+
+export interface DiseaseDetectionJobData {
+  userId: Types.ObjectId;
+  profileId: Types.ObjectId;
+
+  imageUrl: string;
+  imagePublicId: string;
+}
+
+export interface CropRecommendationJobData {
+  userId: Types.ObjectId;
+  profileId: Types.ObjectId;
+
+  inputParameters: CropRecommendationInput;
+}
