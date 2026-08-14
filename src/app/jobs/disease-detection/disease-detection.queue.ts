ddeed83c08322770880e8f9c0f6b481f43fd695f
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-14 12:05:29 +0600

    feat: implement disease detection job queue with job addition functionality

---
+++ src/app/jobs/disease-detection/disease-detection.queue.ts
@@ -0,0 +1,30 @@
+import { Queue, RedisConnection } from 'bullmq';
+import type { DiseaseDetectionJobData } from '../../shared/queue/queue.interface.js';
+import { JOB_NAME, QUEUE_NAME } from '../../shared/queue/queue.constant.js';
+
+export const diseaseDetectionQueue = new Queue<DiseaseDetectionJobData>(
+  QUEUE_NAME.DISEASE_DETECTION,
+  {
+    connection: RedisConnection,
+  }
+);
+
+export const addDiseaseDetectionJob = async (data: DiseaseDetectionJobData) => {
+  return diseaseDetectionQueue.add(JOB_NAME.DISEASE_DETECTION, data, {
+    attempts: 3,
+
+    backoff: {
+      type: 'exponential',
+      delay: 2000,
+    },
+
+    removeOnComplete: {
+      age: 60 * 60,
+      count: 1000,
+    },
+
+    removeOnFail: {
+      age: 24 * 60 * 60,
+    },
+  });
+};
