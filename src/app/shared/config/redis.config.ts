42e2479e39946aa2ab1189f884772cba8e213d8d
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-08-30 12:15:38 +0600

    Update redisConnectionOptions to explicitly include TLS settings for Upstash:

--- src/app/shared/config/redis.config.ts
+++ src/app/shared/config/redis.config.ts
@@ -4,5 +4,8 @@ export const redisConnectionOptions = {
   host: config.REDIS.REDIS_HOST,
   port: config.REDIS.REDIS_PORT,
   password: config.REDIS.REDIS_PASSWORD ?? undefined,
-  tls: {},
+  tls: {
+    rejectUnauthorized: false,
+  },
+  maxRetriesPerRequest: null,
 };
