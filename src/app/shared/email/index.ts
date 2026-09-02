d1bbc658fa87a02d0376a5fdda5166038f6a5f91
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-09-02 10:29:45 +0600

    rename verification email template and update exports in email index

--- src/app/shared/email/index.ts
+++ src/app/shared/email/index.ts
@@ -1,5 +1,5 @@
 export * from './email.constant.js';
 export * from './email.interface.js';
 export * from './email.service.js';
-export * from './email.template.js';
+export * from './verification.template.js';
 export * from './forgot-password.template.js';
