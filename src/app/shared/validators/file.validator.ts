d596bee52e4d8aa3107a8a133451754d5b8c2926
raiyanjiyon <raiyanjiyon814@gmail.com>
2026-09-04 11:18:24 +0600

    refactor: remove outdated comments and streamline file validation logic

--- src/app/shared/validators/file.validator.ts
+++ src/app/shared/validators/file.validator.ts
@@ -1,13 +1,12 @@
-/**
- * File validator utility functions will be written here. These functions will be used to validate the file type, size, and other properties.
- * @module file.validator
- * @author Raiyan Jiyon
- * @version 1.0.0
- * @since 2026-07-06
- * @description This module contains utility functions for validating files.
- * @example
- * import { validateFileType, validateFileSize } from './file.validator';
- *
- * const isValidType = validateFileType(file, ['image/jpeg', 'image/png']);
- * const isValidSize = validateFileSize(file, 5 * 1024 * 1024); // 5MB
- */
+import z from 'zod';
+
+export const fileSchema = z
+  .instanceof(File, {
+    error: 'File is required.',
+  })
+  .refine((file) => file.size <= 5 * 1024 * 1024, {
+    message: 'File size must be less than or equal to 5MB.',
+  })
+  .refine((file) => ['image/jpeg', 'image/png', 'image/gif'].includes(file.type), {
+    message: 'Invalid file type. Only JPEG, PNG, and GIF are allowed.',
+  });
